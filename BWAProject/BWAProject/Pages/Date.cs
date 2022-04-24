65f19c62a9826bb89a14541589427d7a4ef5b38e
KNobuhara <knobuhara@KNobuharanoMacBook-Pro.local>
2022-04-24 17:53:21 +0900

    push Blazor Porj

---
+++ BWAProject/BWAProject/Pages/Date.cs
@@ -0,0 +1,14 @@
+﻿namespace BWAProject.Pages
+{
+	public class Date
+	{
+		/// <summary>カレンダーの日付</summary>
+		public System.DateTime DateTime { get; set; }
+
+		/// <summary>HTMLのclass</summary>
+		public string Class { get; set; } = "";
+
+		/// <summary>HTMLのid</summary>
+		public string Id { get; set; } = "";
+	}
+}
---
+++ BWAProject/BWAProject/Pages/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BWAProject.Pages
+{
+    public class ViewModel : ComponentBase
+    {
+        public List<Dictionary<string, string>> info = new List<Dictionary<string, string>>();
+
+        public ViewModel()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                dic.Add("name","name(" + i + ")");
+                dic.Add("shift", "shift(" + i + ")");
+
+                info.Add(dic);
+            }
+        }
+
+        public List<Dictionary<string, string>> GetInfoList()
+        {
+            return info;
+        }
+
+        public int GetInfoListCount()
+        {
+            return info.Count;
+        }
+    }
+}
