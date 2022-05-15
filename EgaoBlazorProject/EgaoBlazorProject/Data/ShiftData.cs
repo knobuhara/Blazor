457572f791769a784cf42d9fd5ab1824b8110f51
KNobuhara <knobuhara@KNobuharanoMacBook-Pro.local>
2022-05-15 11:25:06 +0900

    add egao project

---
+++ EgaoBlazorProject/EgaoBlazorProject/Data/ShiftData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace EgaoBlazorProject.Data
+{
+    public class ShiftData : ComponentBase
+    {
+        public List<Dictionary<string, string>> info = new List<Dictionary<string, string>>();
+
+        public ShiftData()
+        {
+            // test code for table
+            for (int i = 0; i < 20; i++)
+            {
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                dic.Add("name", "name(" + i + ")");
+                dic.Add("shift", "shift(" + i + ")");
+
+                info.Add(dic);
+            }
+
+            // TODO:MySql
+
+        }
+
+        public List<Dictionary<string, string>> GetInfoList()
+        {
+            return info;
+        }
+
+        public int GetInfoListCount()
+        {
+            return info.Count;
+        }
+    }
+}
